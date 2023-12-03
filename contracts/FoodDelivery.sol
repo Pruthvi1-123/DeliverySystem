2204e5fd31f60b7ca388d4f8554c62d00d97fa4b
Ahmad-385 <2021385@student.cct.ie>
2023-12-03 02:28:21 +0000

    Adding Restaurant and MenuItem structure to FoodDeliverySystem contract

--- contracts/FoodDelivery.sol
+++ contracts/FoodDelivery.sol
@@ -1,2 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.4.0 <0.8.0;
+
+
+// Menu Item and Restaurant  structure
+contract FoodDeliverySystem {
+    struct Restaurant {
+        string name;
+        address owner;
+        mapping(uint256 => MenuItem) menu;
+        uint256 menuCount;
+    }
+
+    struct MenuItem {
+        string name;
+        uint256 price;
+    }
